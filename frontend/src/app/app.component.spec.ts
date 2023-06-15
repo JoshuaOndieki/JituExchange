8248444d77f69df057f2893aa3dd9203f8018f42
Joshua Ondieki <oj@joshuaondieki.com>
2023-06-15 13:47:45 +0300

    [Refactor] Remove unused imports

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app/app.component';
 import { IonicModule } from '@ionic/angular';
